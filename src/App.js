2bd51ad7dd05c0637e218ce9d62c337cb8eea9a3
neeraj162 <ashishneeraj2003@gmail.com>
2022-06-12 13:46:51 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="/">Text Utils</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarsExample04">
+        <ul className="navbar-nav me-auto mb-2 mb-md-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">About</a>
+          </li>
+        </ul>
+        <form role="search">
+          <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+        </form>
+      </div>
     </div>
+  </nav>
+    </>
+    
   );
 }
 
